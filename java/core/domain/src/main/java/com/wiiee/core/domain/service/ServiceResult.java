25714e7558b31da1b15deb346215a7b584220bd4
Bill Wang <billwang@skysznmac025.corp.skyscanner.local>
2018-02-06 18:38:04 +0800

    refactor ServiceResult

--- java/core/domain/src/main/java/com/wiiee/core/domain/service/ServiceResult.java
+++ java/core/domain/src/main/java/com/wiiee/core/domain/service/ServiceResult.java
@@ -4,27 +4,11 @@ package com.wiiee.core.domain.service;
  * Created by wiiee on 9/17/2017.
  */
 public class ServiceResult {
-    private boolean successful;
-    private String message;
+    public boolean isSuccessful;
+    public String message;
 
-    public ServiceResult(boolean successful, String message) {
-        this.successful = successful;
-        this.message = message;
-    }
-
-    public boolean isSuccessful() {
-        return successful;
-    }
-
-    public void setSuccessful(boolean successful) {
-        successful = successful;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
+    public ServiceResult(boolean isSuccessful, String message) {
+        this.isSuccessful = isSuccessful;
         this.message = message;
     }
 }
