01ac7950f0162d0374d536b7585a19162b5fc4d3
Bill Wang <billwang@skysznmac025.corp.skyscanner.local>
2018-02-11 16:43:01 +0800

    set timestamp name to @timestamp to keep the same of kibana's default timefield

--- java/core/platform/src/main/java/com/wiiee/core/platform/log/LogEntry.java
+++ java/core/platform/src/main/java/com/wiiee/core/platform/log/LogEntry.java
@@ -1,9 +1,10 @@
 package com.wiiee.core.platform.log;
 
+import com.google.gson.annotations.SerializedName;
 import com.wiiee.core.platform.context.IContext;
 import com.wiiee.core.platform.util.GsonUtil;
 
-import java.sql.Timestamp;
+import java.time.Instant;
 
 /**
  * Created by wiiee on 9/10/2017.
@@ -34,8 +35,9 @@ public class LogEntry implements ILogEntry {
     public String data;
 
     //时间戳
-    public Timestamp _timestamp;
-    
+    @SerializedName("@timestamp")
+    public String timestamp;
+
     public LogEntry(){}
 
     public LogEntry build(IContext context,
@@ -62,7 +64,7 @@ public class LogEntry implements ILogEntry {
 
         this.data = GsonUtil.toJson(data);
 
-        this._timestamp = new Timestamp(System.currentTimeMillis());
+        this.timestamp = Instant.now().toString();
 
         return this;
     }
