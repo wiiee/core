6f209e27e31610eff7e1d1e35b24734cc67ad2fd
Bill Wang <billwang@skysznmac025.corp.skyscanner.local>
2018-02-22 19:12:41 +0800

    add default constructor for AuthUser

--- java/core/domain/src/main/java/com/wiiee/core/domain/security/AuthUser.java
+++ java/core/domain/src/main/java/com/wiiee/core/domain/security/AuthUser.java
@@ -3,9 +3,4 @@ package com.wiiee.core.domain.security;
 public class AuthUser {
     public String username;
     public String password;
-
-    public AuthUser(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
 }
