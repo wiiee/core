6458aaa1e7b92bfed12dcbf65c78ed040e5152a5
Bill Wang <bill.wang@skyscanner.net>
2018-05-02 19:27:29 +0800

    fix build issue

--- java/core/web/src/main/java/com/wiiee/core/web/api/MyErrorController.java
+++ java/core/web/src/main/java/com/wiiee/core/web/api/MyErrorController.java
@@ -8,8 +8,6 @@ import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestAttributes;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
