447c7c27d578b946008f375d497d17344b873945
Bill Wang <bill.wang@skyscanner.net>
2018-05-08 18:46:19 +0800

    add keyvaluepair

--- java/core/platform/src/main/java/com/wiiee/core/platform/model/KeyValuePair.java
+++ java/core/platform/src/main/java/com/wiiee/core/platform/model/KeyValuePair.java
@@ -3,4 +3,12 @@ package com.wiiee.core.platform.model;
 public class KeyValuePair {
     public String key;
     public String value;
+
+    public KeyValuePair() {
+    }
+
+    public KeyValuePair(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
 }
--- java/core/platform/src/main/java/com/wiiee/core/platform/util/EnumUtil.java
+++ java/core/platform/src/main/java/com/wiiee/core/platform/util/EnumUtil.java
@@ -1,5 +1,6 @@
 package com.wiiee.core.platform.util;
 
+import com.wiiee.core.platform.model.KeyValuePair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,18 +12,18 @@ import java.util.*;
 public abstract class EnumUtil {
     private static final Logger _logger = LoggerFactory.getLogger(EnumUtil.class);
 
-    private static Map<String, List<Map.Entry<String, Object>>> enums;
+    private static Map<String, List<KeyValuePair>> enums;
 
     static {
         enums = new HashMap<>();
     }
 
-    public static List<Map.Entry<String, Object>> getOptions(String name) {
+    public static List<KeyValuePair> getOptions(String name) {
         if (enums.containsKey(name)) {
             return enums.get(name);
         }
 
-        List<Map.Entry<String, Object>> pairs = new ArrayList<>();
+        List<KeyValuePair> pairs = new ArrayList<>();
 
         try {
             Class<?> clazz = Class.forName(name);
@@ -35,7 +36,7 @@ public abstract class EnumUtil {
 
             if (items != null) {
                 for (Object item : items) {
-                    pairs.add(new AbstractMap.SimpleEntry<>(item.toString(), item.toString()));
+                    pairs.add(new KeyValuePair(item.toString(), item.toString()));
                 }
             }
         } catch (Exception ex) {
